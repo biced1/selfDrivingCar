9eab10c7e49e912d835fb6ce08b8c587dde6766f
Kevin <kevin@kevin-ThinkPad-W530.(none)>
2015-04-08 14:33:31 -0600

    added missing files?

--- SelfDrivingCar/src/model/TestWorld.java
+++ SelfDrivingCar/src/model/TestWorld.java
@@ -7,10 +7,10 @@ import control.Road;
 
 public class TestWorld extends World {
 
-	public static int worldWidth = 1500;
+	public static int worldWidth = 1800;
 	public static int worldHeight = 800;
 	public static int cellSize = 1;
-	public static int numberOfRoads = 4;
+	public static int numberOfRoads = 2;
 	public static int roadOffset = 50;
 
 	public TestWorld() {
@@ -25,17 +25,18 @@ public class TestWorld extends World {
 		for (int x = 0; x < numberOfRoads; x++) {
 			for (int y = 0; y < worldWidth / roadSegmentWidth + 1; y++) {
 				int xPos = roadSegmentWidth / 2 + roadSegmentWidth * y;
-				int yPos = (worldHeight / numberOfRoads * x) + roadOffset
+				int yPosCurb1 = (worldHeight / numberOfRoads * x) + roadOffset
 						+ Curb.getCurbWidth() / 2;
-				this.addObject(new Curb(roadSegmentWidth), xPos, yPos);
 
-				yPos = (worldHeight / numberOfRoads * x) + roadOffset
+				int yPosRoad = (worldHeight / numberOfRoads * x) + roadOffset
 						+ Curb.getCurbWidth() + Road.getRoadWidth() / 2;
-				this.addObject(new Road(roadSegmentWidth), xPos, yPos);
-				
-				yPos = (worldHeight / numberOfRoads * x) + roadOffset
+
+				int yPosCurb2 = (worldHeight / numberOfRoads * x) + roadOffset
 						+ Curb.getCurbWidth() + Road.getRoadWidth();
-				this.addObject(new Curb(roadSegmentWidth), xPos, yPos);
+				
+				this.addObject(new Curb(roadSegmentWidth), xPos, yPosCurb1);
+				this.addObject(new Road(roadSegmentWidth), xPos, yPosRoad);
+				this.addObject(new Curb(roadSegmentWidth), xPos, yPosCurb2);
 			}
 		}
 	}
