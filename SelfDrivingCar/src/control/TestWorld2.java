c20f7fdc85485b3222cb088634fa7a7afdfe0539
Kevin <kevin@kevin-ThinkPad-W530.(none)>
2015-04-27 13:48:52 -0600

    car centering works

--- SelfDrivingCar/src/control/TestWorld2.java
+++ SelfDrivingCar/src/control/TestWorld2.java
@@ -1,6 +1,7 @@
 package control;
 
 import greenfoot.World;
+import greenfootAdditions.Vector;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,28 +9,51 @@ import java.util.List;
 import model.Map;
 
 public class TestWorld2 extends World {
-
+	private double viewFrameX = 0;
+	private double viewFrameY = 0;
 	public static int worldWidth = 1900;
 	public static int worldHeight = 900;
 	private static int cellSize = 1;
+	private Map map;
+	private Car currentCenter = new Car();
 	private List<Car> cars = new ArrayList<Car>();
 
 	public TestWorld2() {
 		super(worldWidth, worldHeight, cellSize, false);
 		addMap();
-		addCar(worldWidth/2, worldHeight/2);
+		addCar(worldWidth / 2, worldHeight / 2);
 	}
-	private void addMap(){
-		Map map = new Map(cars);
-		this.addObject(map, map.getImage().getWidth() / 2, map.getImage().getHeight() /2);
+
+	private void addMap() {
+		map = new Map(cars);
+		this.addObject(map, map.getImage().getWidth() / 2, map.getImage()
+				.getHeight() / 2);
 	}
-	
-	private void addCar(int x, int y){
-		Car car = new Car();
+
+	private void addCar(int x, int y) {
+		Car car = currentCenter;
 		this.addObject(car.getFront(), x + 35, y);
 		this.addObject(car.getRear(), x - 35, y);
 		this.addObject(car, x, y);
 		cars.add(car);
 	}
 
+	@Override
+	public void act() {
+		int xMiddle = worldWidth / 2;
+		int yMiddle = worldHeight / 2;
+		double xChange = xMiddle - currentCenter.getExactX();
+		double yChange = yMiddle - currentCenter.getExactY();
+		viewFrameX += xChange;
+		viewFrameY += yChange;
+		map.setLocation(map.getImage().getWidth() / 2 + viewFrameX, map
+				.getImage().getHeight() / 2 + viewFrameY);
+		currentCenter.setLocation(xMiddle, yMiddle);
+		currentCenter.getFront().setLocation(
+				currentCenter.getFront().getExactX() + xChange,
+				currentCenter.getFront().getExactY() + yChange);
+		currentCenter.getRear().setLocation(
+				currentCenter.getRear().getExactX() + xChange,
+				currentCenter.getRear().getExactY() + yChange);
+	}
 }
--- SelfDrivingCar/src/model/Map.java
+++ SelfDrivingCar/src/model/Map.java
@@ -1,15 +1,15 @@
 package model;
 
-import greenfoot.Actor;
 import greenfoot.Greenfoot;
 import greenfoot.GreenfootImage;
+import greenfootAdditions.SmoothMover;
 
 import java.util.List;
 
 import control.Car;
 import control.TestWorld2;
 
-public class Map extends Actor {
+public class Map extends SmoothMover {
 	private static int scale = 4;
 	private static int panSpeed = 10;
 	private List<Car> cars;
@@ -23,26 +23,27 @@ public class Map extends Actor {
 
 	@Override
 	public void act() {
-		if (Greenfoot.isKeyDown("up")) {
-			if (distanceFromTop > 0) {
-				pan(0, panSpeed);
-			}
-		}
-		if (Greenfoot.isKeyDown("down")) {
-			if (distanceFromTop < this.getImage().getHeight() - TestWorld2.worldHeight - panSpeed) {
-				pan(0, -panSpeed);
-			}
-		}
-		if (Greenfoot.isKeyDown("left")) {
-			if (distanceFromLeft > 0) {
-				pan(panSpeed, 0);
-			}
-		}
-		if (Greenfoot.isKeyDown("right")) {
-			if (distanceFromLeft < this.getImage().getWidth() - TestWorld2.worldWidth - panSpeed) {
-				pan(-panSpeed, 0);
-			}
-		}
+//		if (Greenfoot.isKeyDown("up")) {
+//			if (distanceFromTop > 0) {
+//				pan(0, panSpeed);
+//			}
+//		}
+//		if (Greenfoot.isKeyDown("down")) {
+//			if (distanceFromTop < this.getImage().getHeight() - TestWorld2.worldHeight - panSpeed) {
+//				pan(0, -panSpeed);
+//			}
+//		}
+//		if (Greenfoot.isKeyDown("left")) {
+//			if (distanceFromLeft > 0) {
+//				pan(panSpeed, 0);
+//			}
+//		}
+//		if (Greenfoot.isKeyDown("right")) {
+//			if (distanceFromLeft < this.getImage().getWidth() - TestWorld2.worldWidth - panSpeed) {
+//				pan(-panSpeed, 0);
+//			}
+//		}
+		move();
 	}
 	
 	private void setupImage(){
@@ -54,7 +55,7 @@ public class Map extends Actor {
 	private void pan(int x, int y) {
 		distanceFromLeft -= x;
 		distanceFromTop -= y;
-		this.setLocation(this.getX() + x, this.getY() + y);
+		this.setLocation(this.getExactX() + x, this.getExactY() + y);
 		for (Car car : cars) {
 			car.getFront().setLocation(car.getFront().getExactX() + x, car.getFront().getExactY() + y);
 			car.getRear().setLocation(car.getRear().getExactX() + x, car.getRear().getExactY() + y);
